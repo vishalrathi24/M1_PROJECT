80b15bc3cd1c93fc522f608086cfdf53a8ea85f7
Vishal rathi <82195289+vishalrathi24@users.noreply.github.com>
2022-03-28 12:10:02 +0530

    commit

---
+++ 3_implementtion/add_integers.c
@@ -0,0 +1,18 @@
+#include "stdio.h"
+ 
+
+int main()
+{
+ int A=0,B=0;
+
+  printf("enter the number"); 
+ scanf("%d",&A);
+  printf("enter the number");
+ scanf("%d",&B);
+int sum=0;
+
+
+  sum=A+B;
+  printf("sum=%d,sum");
+  return 0;
+} 
